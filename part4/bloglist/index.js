319d1e28ea8a6d492fdb84381d71aaa9173c1d23
Ivan Hu <ivanhu2246@gmail.com>
2021-07-28 19:21:33 +0800

    Refactored into a separate app.js
    4 .5 cumulative hours spent on part 4

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,22 +1,6 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
-
-logger.info('connecting to', config.MONGODB_URI_REDACTED)
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => logger.info("Connected to MongoDB"))
-  .catch((error) => logger.error('error connecting to MongoDB: ', error.message))
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
